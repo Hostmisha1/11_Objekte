52a5e22879fb95ec8d9790da8a84fa46902725bb
hostmisha1 <host.misha@googlemail.com>
2021-08-05 14:16:39 +0200

    Maurice eingefügt und const txt

--- script_01.js
+++ script_01.js
@@ -1,18 +1,24 @@
 
 
 /***** Erinnerung: Arrays *******/
-// let arr;
+ let arr;
 // arr = ["Ich","bin","Max"];
 // ausgabe(arr[2]);
 
-// arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]];
-// ausgabe(arr[0][2]);
-// ausgabe(arr[1][2]);
+ arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]];
+ ausgabe(arr[0][2]);
+ ausgabe(arr[1][2]);
 
 
 /***** Objekte 1 Daten/Funktionen *******/
 
 
+ausgabe(person);
+let person = 
+{ 
+        firstName: "Max",
+        familyName:"Mütze"
+}
 
 /** Ausgabe */
 function ausgabe(outputStr) {
